c3ab3d87a2a127bf2fb5fab71631fe26508d5c4a
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2022-08-19 14:56:16 +0200

    add devcontainer setup

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,14 @@
+# ocaml/opam post create script
+
+sudo chown -R opam: _build
+sudo chown -R opam: node_modules
+
+opam init -a --shell=zsh
+
+# update default ocaml remote - make sure that opam finds latest package versions
+# (e.g. otherwise alcotest latest version is 1.1.0 instead of 1.2.1)
+opam remote remove --all default
+opam repository add default --all-switches --set-default https://opam.ocaml.org
+
+opam pin add -yn guardian .
+opam depext -y guardian
